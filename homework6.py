e007a56b2c42faa848910a6f60d7f8aced423e7f
AlexandrOdintsov <alexodin83@mail.ru>
2024-07-12 00:39:21 +0300

    Add files via upload

---
+++ homework6.py
@@ -0,0 +1,17 @@
+my_dict = {"Alex": 1983, "George": 2002, "Marina": 1980, "Gleb": 2013}
+print(my_dict)
+print(my_dict["Alex"])
+my_dict ["Judie"] = 2022
+print (my_dict)
+my_dict.update({"Vova":1964, "Tatyana":1966})
+print(my_dict)
+my_dict.pop("Alex") #не понял как вывести значение удаленной пары
+print(my_dict)
+
+my_set = {1, 2, 3, 4, 4, 2, 8, 1, 3, 1, "Alex", "John", "Alex", 2.6, 2.6, 2.8, True, False, True}
+print(my_set)
+print (my_set.add(5))
+print (my_set.add("Vova"))
+print (my_set)
+print(my_set.discard(2.6))
+print (my_set)
